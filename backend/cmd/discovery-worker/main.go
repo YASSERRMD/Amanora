4f524db12de9d6a1b2d27ad3fe57453423e0c7f7
YASSERRMD <arafath.yasser@gmail.com>
2026-06-14 05:54:25 +0400

    phase 4: add discovery worker

--- backend/cmd/discovery-worker/main.go
+++ backend/cmd/discovery-worker/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/YASSERRMD/Amanora/backend/internal/worker"
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/YASSERRMD/Amanora/backend/internal/discovery"
+)
 
 func main() {
-	worker.RunUntilSignal("discovery-worker")
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	if err := (discovery.Worker{Logger: logger}).Run(ctx); err != nil && err != context.Canceled {
+		logger.Error("discovery worker failed", "error", err)
+		os.Exit(1)
+	}
 }
