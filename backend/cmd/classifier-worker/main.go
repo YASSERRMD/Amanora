40a921d69277fc0c15b30b312385f39fa0d489de
YASSERRMD <arafath.yasser@gmail.com>
2026-06-14 06:00:53 +0400

    phase 5: add classifier worker

--- backend/cmd/classifier-worker/main.go
+++ backend/cmd/classifier-worker/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/YASSERRMD/Amanora/backend/internal/worker"
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/YASSERRMD/Amanora/backend/internal/classifier"
+)
 
 func main() {
-	worker.RunUntilSignal("classifier-worker")
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	if err := (classifier.Worker{Service: classifier.NewDefaultService(), Logger: logger}).Run(ctx); err != nil && err != context.Canceled {
+		logger.Error("classifier worker failed", "error", err)
+		os.Exit(1)
+	}
 }
